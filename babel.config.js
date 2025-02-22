0e1a9a578f2c96544b3996ef803bcb6ad55e0fe2
caomaxpro <lecao819@gmail.com>
2025-02-21 18:59:14 -0800

    setup 2

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      "module-resolver", 
+      {
+        root: ["./src"],
+        alias: {
+          "@src": './src',
+          "@image": './src/assets/images'
+        }
+      }
+    ],
+      'react-native-reanimated/plugin',
+    ],
 };
--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,17 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const path = require('path');
+const { wrapWithReanimatedMetroConfig } = require('react-native-reanimated/metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
-const config = {};
+console.log('Resolved @src path:', path.resolve(__dirname, 'src'));
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+// Default config for Metro bundler
+const config = {
+  
+};
+
+// Merge default config with your custom config
+const defaultConfig = getDefaultConfig(__dirname);
+const mergedConfig = mergeConfig(defaultConfig, config);
+
+// Wrap with Reanimated-specific Metro config if using Reanimated
+module.exports = wrapWithReanimatedMetroConfig(mergedConfig);
